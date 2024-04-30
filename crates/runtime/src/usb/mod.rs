453896208bf50d07ee4060f1eb7295d2d091b051
Thomas Chiantia <thomas.chiantia@gmail.com>
2024-04-30 11:20:10 -0400

    OpenFuture made public
    All OpenFuture type to be public
    
    Signed-off-by: Thomas Chiantia <thomas.chiantia@gmail.com>

--- crates/runtime/src/usb/mod.rs
+++ crates/runtime/src/usb/mod.rs
@@ -4,7 +4,8 @@ mod open;
 
 // We re-export types used when opening a usb device
 pub use self::open::{
-    Baud, Dcb, DcbFlags, DeviceControlSettings, DtrControl, FlowControl, Parity, RtsControl, Stop,
+    Baud, Dcb, DcbFlags, DeviceControlSettings, DtrControl, FlowControl, OpenFuture, Parity,
+    RtsControl, Stop,
 };
 
 pub fn open<O: Into<std::ffi::OsString>>(port: O) -> std::io::Result<open::OpenFuture> {
